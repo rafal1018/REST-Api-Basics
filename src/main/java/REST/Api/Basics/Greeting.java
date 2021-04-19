f0a9e4a7186c36780168b9ffc61e4d569a2140a6
rafal1018 <rafaljaneczek@o2.pl>
2021-04-19 22:58:53 +0200

    Hello Api

---
+++ src/main/java/REST/Api/Basics/Greeting.java
@@ -0,0 +1,13 @@
+package REST.Api.Basics;
+
+public class Greeting {
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
